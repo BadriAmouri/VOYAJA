774056d8c9f5633fc4f791433997cc315523d1aa
Amouribadri <badriamouri2016@gmail.com>
2024-11-28 10:37:14 +0100

    folder_structure

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./Components/Navbar"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold  text-center mt-20 underline">
+    <h1 className="text-3xl font-bold  text-primary text-center mt-20 underline">
       WELCOME TO VOYAJA CODE 
+     
     </h1>
   )
 }
