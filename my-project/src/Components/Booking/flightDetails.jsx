a4d9ca7f27a29480bb5e7f63c6c9920dcb026c05
BenamgharAmina <amina.benamghar@ensia.edu.dz>
2024-12-09 20:55:48 +0100

    update the booking  details

--- my-project/src/Components/Booking/flightDetails.jsx
+++ my-project/src/Components/Booking/flightDetails.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import flightImage from '../../assets/bookingPics/airline.png';
+import flightImage1 from '../../assets/bookingPics/airline1.png'; // First image
+import flightImage2 from '../../assets/bookingPics/airline2.png'; // Second image
 import plane_icon from '../../assets/bookingPics/plane_icon.png';
+
 const FlightDetails = () => {
   return (
     <div className="flight-details">
       <div className="flight-info">
-
         {/* <img src="https://via.placeholder.com/50" alt="Airline Logo" /> */}
         <div>
-
           <p className="info_airbus">
             Emirates A380 Airbus
             <span className="price_booking">$240</span>
@@ -18,9 +18,12 @@ const FlightDetails = () => {
             Return Wed, Dec 8
             <span className="time_booking">2h 28m</span>
           </p>
-          <img src={flightImage} alt="Flight Image" className="airline" />
 
-
+          {/* Include both images side by side */}
+          <div className="airline-images">
+            <img src={flightImage1} alt="Airline Image 1" className="airline-image" />
+            <img src={flightImage2} alt="Airline Image 2" className="airline-image" />
+          </div>
         </div>
       </div>
       <p className="flight-timing">
@@ -28,8 +31,6 @@ const FlightDetails = () => {
         <img src={plane_icon} alt="plane Icon" className="plane-icon" />
         12:00 PM - Newark (EWR)
       </p>
-
-
     </div>
   );
 };
