91536aabd24213865bf7a1eb19feb1402a734190
Zanoun Nada <nada.zanoun@ensia.edu.dz>
2024-12-10 16:47:24 +0100

    added offer details page and modified navbar

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,15 @@
 import * as React from "react";
-import Dashboard_Agency from "./Page/Dashboard_Agency"
-import Home from "./Page/Home"
-import   AgencyProfile  from "./Page/AgencyProfile"
-
+import Dashboard_Agency from "./Page/Dashboard_Agency";
+import Home from "./Page/Home";
+import AgencyProfile from "./Page/AgencyProfile";
+import OfferDetails from "./Page/OfferDetailsPage";
+import OfferDetailsPage from "./Page/OfferDetailsPage";
+import Header from "./Components/NavBar";
 function App() {
-  
-
   return (
     <div className="App">
-      <AgencyProfile/>
+      <Header />
+      <OfferDetailsPage />
     </div>
   );
 }
--- my-project/src/Components/NavBar.jsx
+++ my-project/src/Components/NavBar.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import "../Style/NavBar.css";
+import logo from "../assets/Artboard 2@2x (2).png";
+import plane from "../assets/plane.png";
+import profile from "../assets/pro.webp";
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header z-10">
       <div className="header-left">
-        
         <nav className="nav-tabs tab">
-          <img src="src\assets\plane.png" alt="" />
-          <a href="/" className="tab_link"> Find trip</a>
-   
+          <a href="/" className="tab_link flex items-center gap-2">
+            <img src={plane} alt="plane" className="plane" />
+            Find trip
+          </a>
         </nav>
       </div>
-      <div className="header-center"><a href="/" className="logo">
-          <img src="src\assets\Artboard 2@2x (2).png" alt="Voyaja Logo" />
-        </a></div>
+      <div className="header-center">
+        <a href="/" className="logo">
+          <img src={logo} alt="Voyaja Logo" />
+        </a>
+      </div>
       <div className="header-right">
         <div className=" nav-tabs favorites">
-          <a href="/" className="favorite">🖤 Favourites</a >
+          <a href="/" className="favorite">
+            🖤 Favourites
+          </a>
         </div>
         <div className="nav-tabs profile">
-          <img src="src\assets\pro.webp" alt="Profile" />
-          <a href="/" className="profileA">Rania Litim</a >
+          <img src={profile} alt="Profile" />
+          <a href="/" className="profileA">
+            Rania Litim
+          </a>
         </div>
       </div>
     </header>
